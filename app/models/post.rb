788c0850187a53ba3f8429870c6a420481f52be3
styx-404 <ahmed.99.tahaa@gmail.com>
2024-05-05 19:28:50 +0300

    added dependent destroy for associations

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,7 +2,13 @@
 
 class Post < ApplicationRecord
   belongs_to :user
-  has_many :reviews
+  has_many :reviews, dependent: :destroy
 
   validates_presence_of :body
+
+=begin
+  As it's not specified what the top posts are.
+  decided to consider the top posts as the posts with reviews ordered by the posts with most reviews number first
+=end
+ scope :top_posts, -> { joins(:reviews).group('posts.id').order('COUNT(reviews.id) DESC') }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
-  has_many :posts
-  has_many :reviews
+  has_many :posts, dependent: :destroy
+  has_many :reviews, dependent: :destroy
 
   validates_presence_of :username
   validates_uniqueness_of :username
